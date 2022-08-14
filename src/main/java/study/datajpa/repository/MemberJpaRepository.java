0935a0a0a99d063668aca943ccbdfe343c021743
Yongje Shin <yongje1211@gmail.com>
2022-08-14 23:31:37 +0900

    순수 JPA 페이징과 정렬

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -48,4 +48,24 @@ public class MemberJpaRepository {
                 .setParameter("age", age)
                 .getResultList();
     }
+
+    public List<Member> findByUsername(String username) {
+        return em.createNamedQuery("Member.findByUsername", Member.class)
+                .setParameter("username", username)
+                .getResultList();
+    }
+
+    public List<Member> findByPage(int age, int offset, int limit) {
+       return em.createQuery("select m from Member m where m.age = :age order by m.username desc", Member.class)
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+               .getResultList();
+    }
+
+    public long totalCount(int age) {
+        return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
 }
